a6f694a89789720c6173e09787e200ef688300a6
Frendsb <202380372@psu.palawan.edu.ph>
2025-12-14 22:13:38 +0800

    database fixed - added test_api.py for testing code

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '27811497e80109982a54cb6fc9d7e055cb492c757f6248aad09f3c63123b43f4'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'cse1'
+app.config['MYSQL_PASSWORD'] = 'Mystery'
+app.config['MYSQL_DB'] = 'cse'
 mysql = MySQL(app)
 
 #JWT
